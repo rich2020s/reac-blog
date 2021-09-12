d350a9da634744616315a636b936ab89c674dd51
Rich Chan <rich@Jasmines-MacBook-Pro.local>
2021-09-12 13:07:02 +0800

    react blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+import About from "./pages/about";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Header from "./components/Headers";
+import Post from "./pages/post";
+import { AuthContext } from "./context";
+import SinglePost from "./pages/single-post";
+import AllPost from "./pages/all-post";
 
-function App() {
+export default function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("user_token")) {
+      setIsLogin(true);
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthContext.Provider value={{ isLogin, setIsLogin }}>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/all-post/:page">
+              <AllPost />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/add-post">
+              <Post />
+            </Route>
+            <Route path="/posts/:id">
+              <SinglePost />
+            </Route>
+          </Switch>
+        </div>
+      </AuthContext.Provider>
+    </Router>
   );
 }
-
-export default App;
